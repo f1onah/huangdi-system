a38e22ff5d43fcc2b3e9298961435469353fadd2
f1onah <huangfeifei433@gmail.com>
2026-04-28 04:54:45 +0800

    Add English redirect to stats page

--- app/stats/page.tsx
+++ app/stats/page.tsx
@@ -1,7 +1,13 @@
 "use client";
 
 import { HuangdiApp } from "@/components/HuangdiApp";
+import { EnglishNavRedirect } from "@/components/EnglishNavRedirect";
 
 export default function StatsPage() {
-  return <HuangdiApp initialTab="stats" />;
+  return (
+    <>
+      <EnglishNavRedirect />
+      <HuangdiApp initialTab="stats" />
+    </>
+  );
 }

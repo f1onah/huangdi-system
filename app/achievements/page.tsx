6bd23db800f192f4900b680c0b65923adda47aad
f1onah <huangfeifei433@gmail.com>
2026-04-28 12:07:18 +0800

    Remove English overlay from achievements page

--- app/achievements/page.tsx
+++ app/achievements/page.tsx
@@ -1,13 +1,7 @@
 "use client";
 
 import { HuangdiApp } from "@/components/HuangdiApp";
-import { EnglishNavRedirect } from "@/components/EnglishNavRedirect";
 
 export default function AchievementsPage() {
-  return (
-    <>
-      <EnglishNavRedirect />
-      <HuangdiApp initialTab="achievements" />
-    </>
-  );
+  return <HuangdiApp initialTab="achievements" />;
 }

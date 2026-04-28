b0012783c438171c22c536efd5365fa78fffd29e
f1onah <huangfeifei433@gmail.com>
2026-04-28 12:06:53 +0800

    Remove English overlay from tasks page

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,13 +1,7 @@
 "use client";
 
 import { HuangdiApp } from "@/components/HuangdiApp";
-import { EnglishNavRedirect } from "@/components/EnglishNavRedirect";
 
 export default function TasksPage() {
-  return (
-    <>
-      <EnglishNavRedirect />
-      <HuangdiApp initialTab="tasks" />
-    </>
-  );
+  return <HuangdiApp initialTab="tasks" />;
 }

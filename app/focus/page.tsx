db50e178b75d2acafa781365ba160c1f7c353a97
f1onah <huangfeifei433@gmail.com>
2026-04-28 12:06:56 +0800

    Remove English overlay from focus page

--- app/focus/page.tsx
+++ app/focus/page.tsx
@@ -1,13 +1,7 @@
 "use client";
 
 import { HuangdiApp } from "@/components/HuangdiApp";
-import { EnglishNavRedirect } from "@/components/EnglishNavRedirect";
 
 export default function FocusPage() {
-  return (
-    <>
-      <EnglishNavRedirect />
-      <HuangdiApp initialTab="focus" />
-    </>
-  );
+  return <HuangdiApp initialTab="focus" />;
 }
